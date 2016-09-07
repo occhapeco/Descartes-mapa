fa64d9e9a998cb8cdf1bb9a89207dd2adf01fa16
occhapeco <occhapecosenai@gmail.com>
2016-09-07 17:11:43 -0300

    Mapa quase finale app

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,5 +1,11 @@
 // Initialize your app
-var myApp = new Framework7();
+var myApp = new Framework7({
+    material: true,
+    pushState: true,
+    animatePages: true,
+    swipePanel: "left",
+    swipePanelActiveArea:40
+});
 
 // Export selectors engine
 var $$ = Dom7;
@@ -7,7 +13,7 @@ var $$ = Dom7;
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
+    dynamicNavbar: true,
 });
 
 // Callbacks to run specific code for specific pages, for example for About page:
